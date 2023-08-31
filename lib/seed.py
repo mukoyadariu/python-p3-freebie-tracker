d61ce4da0663c2c175639ab001d594bc868f6bb2
mukoyadariu <mukoya.darius@student.moringaschool.com>
2023-08-31 10:14:04 +0300

    completed assignment

--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,67 @@
 #!/usr/bin/env python3
 
 # Script goes here!
+from sqlalchemy import create_engine, ForeignKey, Column, Integer, String
+from sqlalchemy.orm import sessionmaker, relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+
+# Create the engine and session
+engine = create_engine('sqlite:///database.db', echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
+
+# Define Freebie class and relationships
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer, primary_key=True)
+    item_name = Column(String)
+    value = Column(Integer)
+    dev_id = Column(Integer, ForeignKey('devs.id'))
+    company_id = Column(Integer, ForeignKey('companies.id'))
+    
+    dev = relationship("Dev", back_populates="freebies")
+    company = relationship("Company", back_populates="freebies")
+
+# Define Company class and relationships
+class Company(Base):
+    __tablename__ = 'companies'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    founding_year = Column(Integer)
+    
+    devs = relationship("Dev", secondary="freebies")
+    freebies = relationship("Freebie", back_populates="company")
+
+    def give_freebie(self, dev, item_name, value):
+        freebie = Freebie(dev=dev, company=self, item_name=item_name, value=value)
+        session.add(freebie)
+        session.commit()
+
+    @classmethod
+    def oldest_company(cls):
+        return session.query(cls).order_by(cls.founding_year).first()
+
+# Define Dev class and relationships
+class Dev(Base):
+    __tablename__ = 'devs'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    
+    companies = relationship("Company", secondary="freebies")
+    freebies = relationship("Freebie", back_populates="dev")
+
+    def received_one(self, item_name):
+        return any(freebie.item_name == item_name for freebie in self.freebies)
+
+    def give_away(self, other_dev, freebie):
+        if freebie.dev == self:
+            freebie.dev = other_dev
+            session.commit()
+
+# Create tables
+Base.metadata.create_all(engine)
